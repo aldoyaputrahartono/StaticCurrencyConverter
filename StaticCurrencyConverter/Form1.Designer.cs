cad9a98f46c8cda9be3cd17466916784a329130a
aldoyaputrahartono <31863229+aldoyaputrahartono@users.noreply.github.com>
2021-03-28 03:55:12 +0700

    Add files via upload

---
+++ StaticCurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace StaticCurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(213, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 92);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Static\r\nCurrency Converter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rupiah",
+            "US Dollar",
+            "Euro",
+            "Ringgit"});
+            this.comboBox1.Location = new System.Drawing.Point(213, 152);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Rupiah",
+            "US Dollar",
+            "Euro",
+            "Ringgit"});
+            this.comboBox2.Location = new System.Drawing.Point(394, 152);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(150, 28);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(213, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mata Uang Asal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(394, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mata Uang Tujuan";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(394, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(150, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(213, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nilai Asal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(394, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nilai Tujuan";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(303, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StaticCurrencyConverter/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaticCurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox1.Text);
+            float conver = (float)i;
+
+            if (comboBox1.SelectedItem == "Rupiah" && comboBox2.SelectedItem == "US Dollar")
+                conver = (float)i / 14415;
+
+            else if (comboBox1.SelectedItem == "US Dollar" && comboBox2.SelectedItem == "Rupiah")
+                conver = (float)i * 14415;
+
+            else if (comboBox1.SelectedItem == "Rupiah" && comboBox2.SelectedItem == "Euro")
+                conver = (float)i / 17045;
+            
+            else if (comboBox1.SelectedItem == "Euro" && comboBox2.SelectedItem == "Rupiah")
+                conver = (float)i * 17045;
+
+            else if (comboBox1.SelectedItem == "Rupiah" && comboBox2.SelectedItem == "Ringgit")
+                conver = (float)i / 3474;
+
+            else if (comboBox1.SelectedItem == "Ringgit" && comboBox2.SelectedItem == "Rupiah")
+                conver = (float)i * 3474;
+
+            else if (comboBox1.SelectedItem == "Ringgit" && comboBox2.SelectedItem == "US Dollar")
+                conver = (float)i / (float)4.15;
+
+            else if (comboBox1.SelectedItem == "US Dollar" && comboBox2.SelectedItem == "Ringgit")
+                conver = (float)i * (float)4.15;
+
+            else if (comboBox1.SelectedItem == "Ringgit" && comboBox2.SelectedItem == "Euro")
+                conver = (float)i / (float)4.90;
+
+            else if (comboBox1.SelectedItem == "Euro" && comboBox2.SelectedItem == "Ringgit")
+                conver = (float)i * (float)4.90;
+
+            else if (comboBox1.SelectedItem == "US Dollar" && comboBox2.SelectedItem == "Euro")
+                conver = (float)i / (float)1.18;
+
+            else if (comboBox1.SelectedItem == "Euro" && comboBox2.SelectedItem == "US Dollar")
+                conver = (float)i * (float)1.18;
+
+            textBox2.Text = conver.ToString();
+        }
+    }
+}
